9ccc31b685da1ba9ffe94fd3041b088536a3fb95
Atrem Chistyakov <artemchityakovv@mail.ru>
2019-07-24 20:53:29 +0300

    added obstacles and restart

--- core/src/com/mygdx/game/Bird.java
+++ core/src/com/mygdx/game/Bird.java
@@ -2,20 +2,21 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Bird {
   private   Texture img;
-   private Vector2 position;
+   Vector2 position;
   private float vy;
   float gravity;
     public Bird(){
         img=new Texture("bird1.png");
         position=new Vector2(100,380);
         vy=0;
-        gravity=-0.7f;
+        gravity=-0.6f;
     }
     public void render(SpriteBatch batch){
         batch.draw(img,position.x ,position.y );
@@ -26,6 +27,10 @@ public class Bird {
         }
      vy+=gravity;
      position.y+=vy;
+    }
+    public void recreate(){
+        position=new Vector2(100,380);
+        vy=0;
     }
 
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,12 +11,17 @@ public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Background bg;
 	Bird bird;
-	
+	Obstacles obstacles;
+	boolean gameOver;
+	Texture restart;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		bg=new Background();
 		bird=new Bird();
+		obstacles=new Obstacles();
+		gameOver=false;
+		restart=new Texture("RestartBtn.png");
 	}
 
 	@Override
@@ -25,15 +31,38 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		bg.render(batch);
-		bird.render(batch);
+		obstacles.render(batch);
+		if(!gameOver) {
+			bird.render(batch);
+		}else {
+			batch.draw(restart,200 ,200 );
+		}
 		batch.end();
 	}
 	public void update(){
      bg.update();
     bird.update();
+    obstacles.update();
+    for(int i=0;i<Obstacles.obs.length;i++){
+    	if(bird.position.x>Obstacles.obs[i].position.x&&bird.position.x<Obstacles.obs[i].position.x+50){
+    		if(! Obstacles.obs[i].emptySpace.contains(bird.position)){
+    			gameOver=true;
+			}
+		}
+		if(bird.position.y>550||bird.position.y<0)gameOver=true;
+    	if(Gdx.input.isKeyPressed(Input.Keys.SPACE)&&gameOver){
+    		gameOver=false;
+    		recreate();
+		}
+	}
 	}
 	@Override
 	public void dispose () {
 		batch.dispose();
+	}
+	public void recreate(){
+		bird.recreate();
+		obstacles.recreate();
+
 	}
 }
